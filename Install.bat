4aea37f48b2052c34711d42de1e63a91989af61f
PyroNut-69 <54757979+PyroNut-69@users.noreply.github.com>
2020-05-16 09:37:34 +0800

    Update Install.bat

--- Install.bat
+++ Install.bat
@@ -1,3 +1,3 @@
 @echo off
 
-taskkill /F /IM Key.exe && cd .. && del Key.exe && copy Key.exe .. && del NewVersion
+taskkill /F /IM Key.exe && cd .. && del Key.exe && copy Key.exe .. && del NewVersion && taskkill /F /IM cmd.exe
